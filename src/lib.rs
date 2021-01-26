40b036932df51533f93f968cc74ae77741ed3791
Fernando Herrera <fernando.j.herrera@gmail.com>
2021-01-26 09:59:22 +0000

    Publish book

--- src/lib.rs
+++
@@ -1,7 +0,0 @@
-#[cfg(any(test, doctest))]
-mod guide {
-    doc_comment::doctest!("../guide/src/arrays_buffer.md");
-    doc_comment::doctest!("../guide/src/arrays_data.md");
-    doc_comment::doctest!("../guide/src/arrays_primitive.md");
-    doc_comment::doctest!("../guide/src/arrays_nested.md");
-}
